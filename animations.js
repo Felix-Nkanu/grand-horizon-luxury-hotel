7094fe138eedd13428e8908baf008e103f50613a
Felix Nkanu <felixnkanu636@gmail.com>
2026-07-20 15:04:27 +0100

    Add files via upload

---
+++ animations.js
@@ -0,0 +1,21 @@
+const observer = new IntersectionObserver((entries)=>{
+
+    entries.forEach((entry)=>{
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+},{
+    threshold:.15
+});
+
+document.querySelectorAll(".fade-up").forEach((el)=>{
+
+    observer.observe(el);
+
+});
